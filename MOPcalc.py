082172fe8d987c220aa0941da9d5456646a46e2c
Fergus Greig <fergus.greig@gmail.com>
2021-04-03 16:55:58 +0100

    Refactoring

--- MOPcalc.py
+++ MOPcalc.py
@@ -1,30 +1,33 @@
 import currentdetails
 
 mortVal = [currentdetails.mortValue]
-intSum = 0
-mortRem = mortVal[0]
-months = 0
-years = 0
 
-while (mortRem >= 0):
-    intAnnual = mortRem*(currentdetails.mortInt/100)
-    intMonth = intAnnual/12
-    mortPaid = currentdetails.mortPaym - intMonth
-    mortRem -= mortPaid
-    intSum += intMonth
-    months += 1
-    if (months == 12):
-        months = 0
-        years += 1
-        mortVal.append(mortRem)
+def mortgageCount(payments):
+    intSum = 0
+    mortRem = mortVal[0]
+    months = 0
+    years = 0
+    while (mortRem >= 0):
+        intAnnual = mortRem*(currentdetails.mortInt/100)
+        intMonth = intAnnual/12
+        mortPaid = payments - intMonth
+        mortRem -= mortPaid
+        intSum += intMonth
+        months += 1
+        if (months == 12):
+            months = 0
+            years += 1
+            mortVal.append(mortRem)
     
+    return [years,months, intSum]
 
-
-def showResult():
-    print("Total Interest paid = £"+ str(intSum))
-    print("Time taken = "+ str(years)+" years and " + str(months)+" months.")
-    for n in mortVal:
-        print(n)
+def showResult(overpayment=0):
+    time = mortgageCount(currentdetails.mortPaym + overpayment)
+    print("For an overpayment of £" + str(overpayment))
+    print("The total interest paid is £"+ str(time[2]))
+    print("The time taken is "+ str(time[0])+" years and " + str(time[1])+" months.")
+#    for n in mortVal:
+#        print(n)
 
 if(__name__ == '__main__'):
     showResult()
